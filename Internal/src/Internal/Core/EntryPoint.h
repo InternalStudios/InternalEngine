3def773cd3676e84d5e2a99b6189f84807e67281
ShazerHD <sfulham1@outlook.com>
2020-06-10 11:54:54 +0930

    Update EntryPoint.h

--- Internal/src/Internal/Core/EntryPoint.h
+++ Internal/src/Internal/Core/EntryPoint.h
@@ -14,21 +14,6 @@
 
 int main(int argc, char* argv[])
 {
-    Internal::WindowData data = {};
-    data.Title = "Window";
-    data.width = 1280;
-    data.height = 720;
-    data.IsFocused = true;
-    Internal::Allocator::Init();
-    Internal::WindowsWindow* window = Internal::Allocator::Allocate<Internal::WindowsWindow>(sizeof(::Internal::WindowsWindow));
-    new (window) Internal::WindowsWindow(data);
-    while (!::Internal::Window::ShouldClose())
-    {
-        window.OnUpdate();
-        glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-    }
-    window->~WindowsWindow();
-    free(window);
+
 
 }
